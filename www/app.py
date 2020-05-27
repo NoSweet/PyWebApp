0f8a83cab436849c809b1294e4548265e533fc15
superme <superme@supermedeiMac.local>
2020-05-27 18:00:01 +0800

    coroweb-2

--- www/app.py
+++ www/app.py
@@ -22,7 +22,56 @@ def init():
 
 init()
 
+#拦截器
+async def logger_factory(app, handler):
+    async def logger(request):
+        #记录日志
+        logging.info('Request: %s %s' % (request.method, request.path))
+        #继续处理请求
+        return await handler(request)
+    return logger
 
+async def response_factory(app, handler):
+    async def response(request):
+        logging.info('Resonse handler...')
+        r = await handler(request)
+        if isinstance(r, web.StreamResponse):
+            return r
+        if isinstance(r, bytes):
+            resp = web.Response(body=r)
+            resp.content_type = 'application/octer-stream'
+            return resp
+        if isinstance(r, str):
+            if r.startswith('rediect:'):
+                return web.HTTPFound(r[9:])
+            resp = web.Response(body=r.encode('utf-8'))
+            resp.content_type = 'text/html;charset=utf-8'
+            return resp
+        if isinstance(r, dict):
+            template = r.get('__temlate__')
+            if template is None:
+                resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
+                resp.content_type = 'application/json;charset=utf-8'
+                return resp
+            else:
+                resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
+                resp.content_type = 'text/html;charset=utf-8'
+                return resp
+        if isinstance(r, int) and r >= 100 and r < 600:
+            return web.Response(body=r)
+        if isinstance(r, tuple) and len(r) == 2:
+            t, _ = r
+            if isinstance(t, int) and t >=100 and t <=600:
+                return web.Response(body=r)
+        
+        #default:
+        resp = web.Response(body=str(r).encode('utf-8'))
+        resp.content_type = 'text/plain;charset=utf-8'
+        return resp
+    return response
+
+
+            
 
 
 
--- www/coroweb.py
+++ www/coroweb.py
@@ -1,4 +1,7 @@
-import asyncio, functools, inspect
+import asyncio, functools, inspect, logging
+from aiohttp import web
+from urllib import parse
+from apis import APIError
 
 def get(path):
     '''
@@ -45,13 +48,13 @@ def get_name_kw_args(fn):
 
 def has_name_kw_args(fn):
     params = inspect.signature(fn).parameters
-    for name, param in params.items():
+    for _, param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
             return True
 
 def has_var_kw_arg(fn):
     params = inspect.signature(fn).parameters
-    for name, param,in params.items():
+    for _, param,in params.items():
         if param.kind == inspect.Parameter.VAR_KEYWORD:
             return True
 
@@ -74,8 +77,89 @@ class RequestHandler(object):
     def __init__(self, app, fn):
         self._app = app
         self._func = fn
-        ...
+        self._has_request_arg = has_request_args(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_name_kw_args = has_name_kw_args(fn)
+        self._named_kw_args = get_name_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
     
     async def __call__(self, request):
-        pass
+        kw = None
+        if self._has_var_kw_arg or self._has_name_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest(text='Missing Content-Type')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    parms = await request.json()
+                    if not isinstance(parms, dict):
+                        return web.HTTPBadRequest(text='JSON body must be object.')
+                    kw = parms
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    parms = await request.post()
+                    kw = dict(**parms)
+                else:
+                    return web.HTTPBadRequest(text='Unsupported Content: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        
+        if kw is None:
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unamed kw:
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            #chek named arg:
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check required kw:
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest(text='Missing argument: %s' % name)
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
+
+#add_route
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ','.join(inspect.signature(fn).parameters.keys())))
+    app.route.add_route(method, path, RequestHandler(app, fn))
     
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        fn = getattr(mod, attr)
+        if callable(fn):
+            method = getattr(fn, '__method__', None)
+            path = getattr(fn, '__route__', None)
+            if method and path:
+                add_route(app, fn)
